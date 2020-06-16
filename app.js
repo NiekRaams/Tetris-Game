d0a70b78bcd48c09f85818e5b4091152db5ce408
NiekRaams <66281221+NiekRaams@users.noreply.github.com>
2020-06-16 23:29:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-//construction 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
+
+  //Vormpies
+  const Lvorm = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+  const zvorm = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+  const tvorm = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+  const ovorm = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+  const ivorm = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+
+  //theTetrominoes = vormpies
+  const vormpies = [Lvorm, ivorm, ovorm, zvorm, tvorm]
+  let currentPosition = 4
+
+  //randomly generate a vormpie to make it hard ;)
+  let random = Math.floor(Math.random() * vormpies.length)
+  console.log(random)
+  let current = vormpies[0][0]
+
+
+  //draw the first rotation in the first vorm 
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('vorm')
+    })
+  }
+
+  draw()
+
+
+})
